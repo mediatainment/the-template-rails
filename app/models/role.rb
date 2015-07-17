429dd326216a08e036a525ee3c24cc449d03d649
Jan Jezek <mail@mediatainment-productions.com>
2015-07-18 00:17:10 +0200

    prevent admin to be deleted

--- app/models/role.rb
+++ app/models/role.rb
@@ -8,5 +8,14 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   validates_presence_of :name
+  before_destroy :do_not_change_admin_role
+  before_update :do_not_change_admin_role
+
+  private
+
+  def do_not_change_admin_role
+    errors.add(:base, "Admin Role cannot be changed") if name == 'admin' || name_change.include?('admin')
+    errors.blank? #return false, to not destroy the element, otherwise, it will delete.
+  end
 
 end
