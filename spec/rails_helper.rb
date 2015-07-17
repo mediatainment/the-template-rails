95779a934ef5ac9a1cf0b9f45412e67fdbef270b
Jan Jezek <mail@mediatainment-productions.com>
2015-07-17 18:07:21 +0200

    devise test helper included

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,7 @@ require 'faker'
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Devise::TestHelpers, :type => :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
