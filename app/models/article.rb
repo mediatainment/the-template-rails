eae4c3f3fb0c007af30733820ec9c67b8c78524a
Jan Jezek <mail@mediatainment-productions.com>
2015-07-18 02:01:19 +0200

    add slug to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ActiveRecord::Base
+  extend FriendlyId
+
   attr_accessible :content, :featured, :meta_description, :published, :subtitle, :title
 
   translates :title, :content
+
+  friendly_id :title, use: :slugged
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150717162700) do
+ActiveRecord::Schema.define(:version => 20150717235951) do
 
   create_table "article_translations", :force => true do |t|
     t.integer  "article_id"
@@ -34,7 +34,10 @@ ActiveRecord::Schema.define(:version => 20150717162700) do
     t.string   "meta_description"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "slug"
   end
+
+  add_index "articles", ["slug"], :name => "index_articles_on_slug", :unique => true
 
   create_table "categories", :force => true do |t|
     t.string   "name"
